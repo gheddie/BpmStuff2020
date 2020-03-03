bad97a859148fc3deab1ffcacd569234382cb803
Sts <Sts@view-localhost>
2020-03-03 20:59:23 +0100

    123

--- src/test/java/org/camunda/bpm/unittest/PsychatryProcessTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/PsychatryProcessTestCase.java
@@ -3,9 +3,8 @@ package org.camunda.bpm.unittest;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
 
-import java.util.List;
+import java.util.HashMap;
 
-import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.unittest.base.BpmTestCase;
@@ -19,19 +18,23 @@ public class PsychatryProcessTestCase extends BpmTestCase {
 	
 	@Test
 	@Deployment(resources = { "psychatry/psychatryProcess.bpmn" })
-	public void testExternalAdmission() {
-		
-		runtimeService().startProcessInstanceByMessage("MSG_ADMISSION");
-		
-		taskService().complete(ensureSingleTaskPresent("TaskGatherData").getId());
-		
-		taskService().complete(ensureSingleTaskPresent("TaskReviewData").getId());
-		
-		List<Task> tasks = taskService().createTaskQuery().list();
-		
-		// ---
-		
-		// ensureSingleTaskPresent("TaskChooseMeal");
+	public void testAdmissionGranted() {
+		runProcess(true);
+		ensureSingleTaskPresent("TaskChooseMeal");
+	}
+	
+	@Test
+	@Deployment(resources = { "psychatry/psychatryProcess.bpmn" })
+	public void testAdmissionRevoked() {
+		runProcess(false);
 		ensureSingleTaskPresent("TaskReleasePatient");
+	}
+
+	private void runProcess(boolean costTakenOver) {
+		runtimeService().startProcessInstanceByMessage("MSG_ADMISSION");
+		taskService().complete(ensureSingleTaskPresent("TaskGatherData").getId());
+		HashMap<String, Object> variables = new HashMap<String, Object>();
+		variables.put("costTakenOver", costTakenOver);
+		taskService().complete(ensureSingleTaskPresent("TaskReviewData").getId(), variables);
 	}
 }
--- src/test/java/org/camunda/bpm/unittest/delegate/psychatry/AnswerTakeOverDelegate.java
+++ src/test/java/org/camunda/bpm/unittest/delegate/psychatry/AnswerTakeOverDelegate.java
@@ -10,7 +10,7 @@ public class AnswerTakeOverDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		HashMap<String, Object> variables = new HashMap<String, Object>();
-		variables.put("costTakenOver", false);
+		variables.put("costTakenOver", (boolean) execution.getVariable("costTakenOver"));
 		execution.getProcessEngine().getRuntimeService().correlateMessage("MSG_REQ_PROCESSED", new HashMap<String, Object>(), variables);
 	}
 }
