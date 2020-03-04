42751eb534fef84103f30feb6db4d857b878e5c7
Sts <Sts@view-localhost>
2020-03-04 10:49:19 +0100

    123

--- src/test/java/org/camunda/bpm/unittest/PsychatryProcessTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/PsychatryProcessTestCase.java
@@ -15,28 +15,38 @@ public class PsychatryProcessTestCase extends BpmTestCase {
 
 	@Rule
 	public ProcessEngineRule rule = new ProcessEngineRule();
+	
+	// messages
+	private static final String MSG_ADMISSION = "MSG_ADMISSION";
+	
+	// tasks
+	private static final String TASK_CHOOSE_MEAL = "TaskChooseMeal";
+	private static final String TASK_GATHER_DATA = "TaskGatherData";
+	private static final String TASK_REVIEW_DATA = "TaskReviewData";
+	private static final String TASK_RELEASE_PATIENT = "TaskReleasePatient";
 
 	@Test
 	@Deployment(resources = { "psychatry/psychatryProcess.bpmn" })
-	public void testAdmissions() {
+	public void testMultipleAdmissions() {
 
 		runProcess(false, false, 3);
+
 		runProcess(true, false, 5);
 		runProcess(true, true, 2);
 
-		ensureTaskCountPresent("TaskReleasePatient", 3);
-		ensureTaskCountPresent("TaskChooseMeal", 5);
-		ensureTaskCountPresent("TaskReviewData", 2);
+		ensureTaskCountPresent(TASK_RELEASE_PATIENT, 3);
+		ensureTaskCountPresent(TASK_CHOOSE_MEAL, 5);
+		ensureTaskCountPresent(TASK_REVIEW_DATA, 2);
 	}
 
 	private void runProcess(boolean costTakenOver, boolean stopAtReview, int processCount) {
 		for (int count = 0; count < processCount; count++) {
-			runtimeService().startProcessInstanceByMessage("MSG_ADMISSION");
-			taskService().complete(ensureSingleTaskPresent("TaskGatherData").getId());
+			runtimeService().startProcessInstanceByMessage(MSG_ADMISSION);
+			taskService().complete(ensureSingleTaskPresent(TASK_GATHER_DATA).getId());
 			HashMap<String, Object> variables = new HashMap<String, Object>();
 			variables.put("costTakenOver", costTakenOver);
 			if (!(stopAtReview)) {
-				taskService().complete(ensureSingleTaskPresent("TaskReviewData").getId(), variables);
+				taskService().complete(ensureSingleTaskPresent(TASK_REVIEW_DATA).getId(), variables);
 			}
 		}
 	}
